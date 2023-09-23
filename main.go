353bcb819e8c5977bcb9995ccbdeb22d0745ad2c
Biko <biko59200@gmail.com>
2023-09-23 14:07:59 +0300

    implemented connecting to the database

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
+var db *sql.DB
+
+
 func main() {
+	// database configuration section
+	cfg := mysql.Config {
+		User: "root",
+		Passwd: "Bikobiko59",
+		Net: "tcp",
+		Addr:   "127.0.0.1:3306",
+        DBName: "savings",
+	}
+	// get a database handle
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Mysql connected successfully")
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, Nick")
 	})
 	fmt.Println("Started server successfully at http://localhost:8080")
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
+	
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
--- models/user.go
+++ models/user.go
@@ -2,10 +2,10 @@ package models
 
 type User struct {
 	// the id product of the user
-	ID int `json:"id"`
+	ID        int    `json:"id"`
 	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
 
